f77bec764799f628646fab64ba3e6a8cbe9a1716
Beethoven <beethoven_hv@hotmail.com>
2022-05-27 17:41:15 -0300

    update

--- fundamentos-js/src/index.js
+++ fundamentos-js/src/index.js
@@ -21,7 +21,19 @@ function verifyCPF(request,response,next){
     request.customer = customer;
 
     return next();
-}
+};
+
+function getBalance(statement){
+  const balance = statement.reduce((acc,operation)=>{
+        if(operation.type === 'credit'){
+            return acc + operation.amount;
+        }else{
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+};
 
 app.post("/account",(request,response)=>{
     const {cpf,name} = request.body;
@@ -65,6 +77,38 @@ app.post("/deposit",verifyCPF,(request,response)=>{
     customer.statement.push(statementOperation);
 
     return response.status(201).send();
+});
+
+app.post("/withdraw",verifyCPF,(request,response)=>{
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount){
+        return response.status(400).json({error: "Insufficient Funds!"});
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
+app.get("/statement/date",verifyCPF,(request,response)=>{
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+    return response.json(statement);
 });
 
 app.listen(3333);

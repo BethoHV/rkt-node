5c90ced5e52472ac047d938d191555e12fec986f
BethoHV <beethoven_hv@hotmail.com>
2022-05-30 13:47:51 -0300

    a

--- fundamentos-js/src/index.js
+++ fundamentos-js/src/index.js
@@ -1,114 +1,29 @@
-const { request, response, json } = require('express');
 const express = require('express');
-const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 
 app.use(express.json());
 
-const customers = [];
-
-//Middleware
-function verifyCPF(request,response,next){
-    const { cpf } = request.headers;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if(!customer){
-        return response.status(400).json({error: "Customer not found"});
-    }
-
-    request.customer = customer;
-
-    return next();
-};
-
-function getBalance(statement){
-  const balance = statement.reduce((acc,operation)=>{
-        if(operation.type === 'credit'){
-            return acc + operation.amount;
-        }else{
-            return acc - operation.amount;
-        }
-    }, 0);
-
-    return balance;
-};
-
-app.post("/account",(request,response)=>{
-    const {cpf,name} = request.body;
-
-    const customerAlreadyExists = customers.some(
-        (customer) => customer.cpf === cpf
-    );
-
-    if(customerAlreadyExists){
-        return response.status(400).json({error: "Customer already exists!"});
-    }
-
-    customers.push({
-        cpf,
-        name,
-        id: uuidv4(),
-        statement: []
-    });
-
-    return response.status(201).send();
+app.get("/courses",(request,response)=>{
+    return response.json(["Curso 1","Curso 2", "Curso 3"]);
 });
 
-app.get("/statement",verifyCPF,(request,response)=>{
-    const { customer } = request;
-
-    return response.json(customer.statement);
+app.post("/courses",(request,response)=>{
+    return response.json(["Curso 1","Curso 2", "Curso 3", "Curso 4"]);
 });
 
-app.post("/deposit",verifyCPF,(request,response)=>{
-    const { description, amount } = request.body;
-
-    const { customer } = request;
-
-    const statementOperation = {
-        description,
-        amount,
-        created_at: new Date(),
-        type: "credit"
-    }
-
-    customer.statement.push(statementOperation);
-
-    return response.status(201).send();
+app.put("/courses/:id",(request,response)=>{
+    const params = request.params;
+    console.log(params);
+    return response.json(["Curso 6","Curso 2", "Curso 3", "Curso 4"]);
 });
 
-app.post("/withdraw",verifyCPF,(request,response)=>{
-    const { amount } = request.body;
-    const { customer } = request;
-
-    const balance = getBalance(customer.statement);
-
-    if(balance < amount){
-        return response.status(400).json({error: "Insufficient Funds!"});
-    }
-
-    const statementOperation = {
-        amount,
-        created_at: new Date(),
-        type: "debit",
-    };
-
-    customer.statement.push(statementOperation);
-
-    return response.status(201).send();
+app.patch("/courses/:id",(request,response)=>{
+    return response.json(["Curso 6","Curso 7", "Curso 3", "Curso 4"]);
 });
 
-app.get("/statement/date",verifyCPF,(request,response)=>{
-    const { customer } = request;
-    const { date } = request.query;
-
-    const dateFormat = new Date(date + " 00:00");
-
-    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
-
-    return response.json(statement);
+app.delete("/courses/:id",(request,response)=>{
+    return response.json(["Curso 6","Curso 7","Curso 4"]);
 });
 
 app.listen(3333);
